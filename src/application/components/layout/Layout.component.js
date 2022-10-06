5668f7f360b0e308f75be1f45a29784b33a6c933
Manuel Santiago Gavilan <manuel.santiagogavilan@plexus.local>
2022-10-06 12:50:52 +0200

    feat(header): Create header component
    - Installed material ui and material icons
    - Created header component
    - Added header component to layout
    - Added logo
    - Added cart
    
    Closes #8

--- src/application/components/layout/Layout.component.js
+++ src/application/components/layout/Layout.component.js
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router';
+import Header from './components/header/Header.component';
 import { MainContainer } from './Layout.styled';
+
 const Layout = () => {
   return (
     <MainContainer>
+      <Header />
       <Outlet />
     </MainContainer>
   );
