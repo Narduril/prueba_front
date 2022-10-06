267852ac2d7bb791548799ea3bde0c0da0546d52
Manuel Santiago Gavilan <manuel.santiagogavilan@plexus.local>
2022-10-06 09:26:28 +0200

    feat(routes): Configure routes
    - Added useRoutes hook
    - Configured all routes
    - Created basic pages
    - Created basic layout
    
    Closes #4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import Application from './application/Application.component';
 
 const container = document.getElementById('root');
 
 createRoot(container).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <Application />
-  </BrowserRouter>
+  </React.StrictMode>
 );
---
+++ src/pages/product-details/ProductDetails.component.js
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+  return (
+    <>
+      <h1>Product Details Page</h1>
+    </>
+  );
+};
+
+export default ProductDetails;
